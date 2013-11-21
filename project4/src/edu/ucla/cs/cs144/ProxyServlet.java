e247707700160a6b3a8fe1381ead6d1eff8a4a54
Kevin ChanYoung Kim <kkim0211@gmail.com>
2013-11-21 04:28:33 -0800

    Added proxyservlet code

--- project4/src/edu/ucla/cs/cs144/ProxyServlet.java
+++ project4/src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -1,7 +1,11 @@
 package edu.ucla.cs.cs144;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +18,38 @@ public class ProxyServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+	PrintWriter out = response.getWriter();
+	URL google_suggest = null;
+	BufferedReader reader = null;
+	StringBuilder stringBuilder;
+
+	String q = request.getParameter("q");
+
+	try
+	{
+	  google_suggest = new URL("http://google.com/complete/search?output=toolbar&q="+q);
+	  HttpURLConnection connection = (HttpURLConnection) google_suggest.openConnection();
+	  connection.setRequestMethod("GET");
+	  connection.setReadTimeout(15*1000);
+	  connection.connect();
+
+	  reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	  stringBuilder = new StringBuilder();
+
+	  String line = null;
+	  while ((line = reader.readLine()) != null)
+	  {
+		stringBuilder.append(line + "\n");
+	  }
+	  out.println(stringBuilder.toString());
+	  out.close();
+	  
+	}
+	catch (IOException e)
+	{
+	  e.printStackTrace();
+	  throw e;
+	}	
+	
     }
 }
